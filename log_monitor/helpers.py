e82d1f90941cbf13f2db988cd79dd84720126817
bernard <bvdv9075@gmail.com>
2023-02-08 12:28:33 +0000

    Testing env vars

--- log_monitor/helpers.py
+++ log_monitor/helpers.py
@@ -4,11 +4,11 @@ import os
 
 def create_db_con():
     connection = psycopg2.connect(
-            host=os.getenv("HOST"),
-            database=os.getenv("DB"),
-            user=os.getenv("USERNAME"),
-            password=os.getenv("PASSWORD"),
-            port=os.getenv("PORT"),
+            host=os.getenv("HOST_AWS"),
+            database=os.getenv("DB_AWS"),
+            user=os.getenv("USERNAME_AWS"),
+            password=os.getenv("PASSWORD_AWS"),
+            port=os.getenv("PORT_AWS"),
             sslmode="disable"
         )
     return connection.cursor()
