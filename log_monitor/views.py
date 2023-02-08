69d3178d1b9c674f61421f45818eed27864cd819
bernard <bvdv9075@gmail.com>
2023-02-08 11:44:26 +0000

    Changed template

--- log_monitor/views.py
+++ log_monitor/views.py
@@ -1,12 +1,10 @@
-from django.contrib.auth import authenticate, login
-from django.db.models import Sum
-from django.http import JsonResponse
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import redirect, render
 from django.views.generic import TemplateView, View
 
 from .forms import LoginForm
-# from .helpers import get_month_param, get_months, get_selected_month
-# from .models import BaseConfig, Income, Items, UserSettings
+from .helpers import create_db_con
 
 
 class LoginPageView(View):
@@ -34,3 +32,116 @@ class LoginPageView(View):
 
 class HomePageView(TemplateView):
     template_name = 'log_monitor/main.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cursor = create_db_con()
+
+        retailer_count_query = "select count(DISTINCT retailer) FROM public.statistics;"
+        all_products_query = """SELECT SUM(CAST (itemcount AS INTEGER)) 
+                                FROM public.statistics where created = current_date"""
+        error_count_today_query = "select count(message) from public.log where created::date = now()::date;"
+        stats_query = """select created, retailer, itemcount, "itemCountYesterday", expectedlinks, navigationlinks,
+                        duration from statistics where created = current_date"""
+
+        cursor.execute(retailer_count_query)
+        retailer_records = cursor.fetchall()
+
+        cursor.execute(all_products_query)
+        all_records_count = cursor.fetchall()
+
+        cursor.execute(error_count_today_query)
+        today_error_count = cursor.fetchall()
+
+        cursor.execute(stats_query)
+        stats_records = cursor.fetchall()
+
+        context['retailer_count'] = retailer_records[0][0]
+        context['all_records_count'] = all_records_count[0][0]
+        context['today_error_count'] = today_error_count[0][0]
+        context['stats_records'] = stats_records
+        return context
+
+    def get_total_product_count_last_10(request):
+        last_10_count_products = """
+                SELECT created, SUM(CAST (itemcount AS INTEGER))  FROM public.statistics
+                where created > current_date - interval '10' day group by created;
+                """
+        cursor = create_db_con()
+        cursor.execute(last_10_count_products)
+        product_count_last_10_records = cursor.fetchall()
+        months = []
+        count = []
+        for x, y in product_count_last_10_records:
+            months.append(x)
+            count.append(y)
+        data = {
+            'months': months,
+            'label': count,
+        }
+
+        return JsonResponse(data)
+
+    def get_yesterday_today_imports(request):
+        yesterday_today_imports = """
+                select retailer, itemcount, "itemCountYesterday" from statistics where created = current_date
+                """
+
+        cursor = create_db_con()
+        cursor.execute(yesterday_today_imports)
+        product_count_last_10_records = cursor.fetchall()
+
+        retailer = []
+        today = []
+        yesterday = []
+        for y in product_count_last_10_records:
+            if y[0] not in retailer:
+                retailer.append(y[0])
+                today.append(y[1])
+                yesterday.append(y[2])
+
+        data = {
+            'retailer': retailer,
+            'today': today,
+            'yesterday': yesterday
+        }
+        return JsonResponse(data)
+
+
+class LogsPageView(TemplateView):
+    template_name = 'log_monitor/logs.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        logs_query = """
+        SELECT created, name, level, message, function, lineno FROM public.log order by created desc limit 100;
+        """
+        cursor = create_db_con()
+        cursor.execute(logs_query)
+        logs_last_100 = cursor.fetchall()
+
+        context['logs_last_100'] = logs_last_100
+        return context
+
+
+class StatsPageView(TemplateView):
+    template_name = 'log_monitor/stats.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        stats_query = """
+        select created, retailer, itemcount, expectedlinks, navigationlinks,
+                        duration from statistics order by created desc limit 100
+        """
+        cursor = create_db_con()
+        cursor.execute(stats_query)
+        stats_limit_100 = cursor.fetchall()
+
+        context['stats_limit_100'] = stats_limit_100
+        return context
+
+
+class CronPageView(TemplateView):
+    template_name = 'log_monitor/cron.html'
