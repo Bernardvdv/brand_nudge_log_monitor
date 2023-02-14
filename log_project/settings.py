dba27516557f4d7460f6e284a25f22821d38a24c
bernard <bvdv9075@gmail.com>
2023-02-14 07:29:08 +0000

    fixed date bug

--- log_project/settings.py
+++ log_project/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "yu+1aqv8qb)tab@7ca0!ol4*g=!h57pv#tep910pf&xol#i0m="
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = "yu+1aqv8qb)tab@7ca0!ol4*g=!h57pv#tep910pf&xol#i0m="
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
